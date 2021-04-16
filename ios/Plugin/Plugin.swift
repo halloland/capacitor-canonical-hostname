94b43178beda49dccfea2903dd59f452caf3366a
hmishyn <misgena@gmail.com>
2021-04-16 15:37:00 +0300

    Add reverse DNS lookup for IOS.

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -8,10 +8,45 @@ import Capacitor
 @objc(CanonicalHostname)
 public class CanonicalHostname: CAPPlugin {
 
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
+    @objc func getCanonicalHostname(_ call: CAPPluginCall) {
+        let addressStr = call.getString("ip") ?? ""
+
+        let res = reverseDNS(ip: addressStr)
         call.success([
-            "value": value
+            "hostname": res
         ])
+    }
+    
+    func reverseDNS(ip: String) -> String {
+        var results: UnsafeMutablePointer<addrinfo>? = nil
+        defer {
+            if let results = results {
+                freeaddrinfo(results)
+            }
+        }
+        let error = getaddrinfo(ip, nil, nil, &results)
+        if (error != 0) {
+            print("Unable to reverse ip: \(ip)")
+            return ip
+        }
+
+        for addrinfo in sequence(first: results, next: { $0?.pointee.ai_next }) {
+            guard let pointee = addrinfo?.pointee else {
+                print("Unable to reverse ip: \(ip)")
+                return ip
+            }
+
+            let hname = UnsafeMutablePointer<Int8>.allocate(capacity: Int(NI_MAXHOST))
+            defer {
+                hname.deallocate()
+            }
+            let error = getnameinfo(pointee.ai_addr, pointee.ai_addrlen, hname, socklen_t(NI_MAXHOST), nil, 0, 0)
+            if (error != 0) {
+                continue
+            }
+            return String(cString: hname)
+        }
+
+        return ip
     }
 }
